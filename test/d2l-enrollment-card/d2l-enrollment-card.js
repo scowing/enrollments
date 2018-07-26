966343dd13f36d288249884d6179a968ea7df051
Ryan Mahler <ryan.mahler@d2l.com>
2018-07-26 17:42:42 -0400

    US97555 Remove image overlay
    The `d2l-organization-info` component now supports showing additional information for future/past/inactive courses, so the image overlay is being retired. Unfortunately, there is still some remaining date-related code here, to allow us to set the `past-course` attribute on the card, as well as greying out inaccessible courses. There is probably a better way to do this, but the implementation of this behaviour in `d2l-organization-info` isn't quite complete yet anyway - we are still missing the status badge, which will slightly modify the available information.

--- test/d2l-enrollment-card/d2l-enrollment-card.js
+++ test/d2l-enrollment-card/d2l-enrollment-card.js
@@ -270,15 +270,6 @@ describe('d2l-enrollment-card', () => {
 			expect(component._courseSettingsLabel).to.equal('Course name course settings');
 		});
 
-		it('should update _overlayAnnounceText', () => {
-			var spy = sandbox.spy(component, '_computeOverlayAnnounceText');
-
-			component._organization = organizationEntity;
-
-			expect(spy).to.have.been.called;
-			expect(component._overlayAnnounceText).to.match(/Opens on/);
-		});
-
 		it('should update _pinButtonLabel', () => {
 			var spy = sandbox.spy(component, '_computePinButtonLabel');
 
@@ -379,7 +370,7 @@ describe('d2l-enrollment-card', () => {
 				}
 			}));
 
-			var imageOverlay = component.$$('div.overlay:nth-of-type(3)');
+			var imageOverlay = component.$$('div.overlay');
 			expect(imageOverlay.hasAttribute('hidden')).to.be.false;
 			var spinner = component.$$('.overlay d2l-loading-spinner');
 			expect(spinner.hasAttribute('hidden')).to.be.false;
@@ -396,7 +387,7 @@ describe('d2l-enrollment-card', () => {
 			}));
 
 			clock.tick(1001);
-			var imageOverlay = component.$$('div.overlay:nth-of-type(3)');
+			var imageOverlay = component.$$('div.overlay');
 			expect(imageOverlay.hasAttribute('hidden')).to.be.false;
 			var spinner = component.$$('.overlay d2l-loading-spinner');
 			expect(spinner.hasAttribute('hidden')).to.be.true;
@@ -419,7 +410,7 @@ describe('d2l-enrollment-card', () => {
 			}));
 
 			clock.tick(1001);
-			var imageOverlay = component.$$('div.overlay:nth-of-type(3)');
+			var imageOverlay = component.$$('div.overlay');
 			expect(imageOverlay.hasAttribute('hidden')).to.be.false;
 			var spinner = component.$$('.overlay d2l-loading-spinner');
 			expect(spinner.hasAttribute('hidden')).to.be.true;
@@ -429,70 +420,6 @@ describe('d2l-enrollment-card', () => {
 			clock.tick(1001);
 			expect(imageOverlay.hasAttribute('hidden')).to.be.true;
 			clock.restore();
-		});
-
-	});
-
-	describe('Notification Overlay', () => {
-
-		var futureDate = new Date(3000, 0, 1, 15, 5).toISOString(),
-			pastDate = new Date(1900, 3, 30, 4, 38).toISOString(),
-			formattedFutureDateTime = 'Jan 1, 3000 3:05 PM',
-			formattedPastDateTime = 'Apr 30, 1900 4:38 AM';
-
-		[
-			{ start: futureDate, end: futureDate, active: false },
-			{ start: futureDate, end: futureDate, active: true },
-			{ start: pastDate, end: pastDate, active: false },
-			{ start: pastDate, end: pastDate, active: true },
-			{ start: pastDate, end: futureDate, active: false },
-			{ start: pastDate, end: futureDate, active: true },
-			{ start: null, end: futureDate, active: false },
-			{ start: null, end: futureDate, active: true }
-		].forEach(testCase => {
-
-			it(`${testCase.start}, ${testCase.end}, ${testCase.active}`, () => {
-				organizationEntity.properties.startDate = testCase.start;
-				organizationEntity.properties.endDate = testCase.end;
-				organizationEntity.properties.isActive = testCase.active;
-				component._organization = organizationEntity;
-
-				var overlay = component.$$('div.overlay:nth-of-type(2):not([hidden])');
-				if (
-					testCase.end === futureDate
-					&& (testCase.start === pastDate || !testCase.start)
-					&& testCase.active
-				) {
-					expect(overlay).to.be.null;
-					return;
-				}
-				expect(overlay).to.not.be.null;
-
-				var overlayTitleText = overlay.querySelector('.overlay-text').innerText;
-				var overlayDateText = overlay.querySelector('.overlay-date').innerText;
-				var overlayInactiveText = overlay.querySelector('.overlay-date + div').innerText;
-
-				if (testCase.start === futureDate) {
-					expect(overlayTitleText).to.equal('Opens On');
-					expect(overlayDateText).to.equal(formattedFutureDateTime);
-				} else if (testCase.end === pastDate) {
-					expect(overlayTitleText).to.equal('Closed');
-					expect(overlayDateText).to.equal(formattedPastDateTime);
-				} else {
-					expect(overlayTitleText).to.be.empty;
-					expect(overlayDateText).to.be.empty;
-				}
-
-				if (testCase.active || testCase.end === pastDate) {
-					expect(overlayInactiveText).to.be.empty;
-				} else if (testCase.start === futureDate) {
-					expect(overlayInactiveText).to.equal('(Inactive)');
-				} else {
-					expect(overlayInactiveText).to.equal('Inactive');
-				}
-
-			});
-
 		});
 
 	});
