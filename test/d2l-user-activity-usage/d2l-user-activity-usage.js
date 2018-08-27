e69d2a1cb70f4838cfe6033602f061a0d7c2e806
Maya Jones <Maya.Jones@D2L.com>
2018-08-27 12:30:03 -0400

    Added unit tests to test d2l-user-actiity-usage._sirenClassProperty

--- test/d2l-user-activity-usage/d2l-user-activity-usage.js
+++ test/d2l-user-activity-usage/d2l-user-activity-usage.js
@@ -289,4 +289,84 @@ describe('d2l-user-activity-usage', () => {
 
 	});
 
+	describe('Due Date correctly displayed', () => {
+		it('Read a date', () => {
+			var entity =  window.D2L.Hypermedia.Siren.Parse({
+				entities: [
+					{
+						class: [
+							'date',
+							'due-date'
+						],
+						rel: [
+							'https://api.brightspace.com/rels/date'
+						],
+						properties: {
+							date: '2100-08-01T04:00:00.000Z'
+						}
+					}
+				]
+			});
+
+			expect(component._sirenClassProperty(entity, 'due-date')).to.equal('2100-08-01T04:00:00.000Z');
+
+		});
+
+		it('Read a duration', () => {
+			var entity =  window.D2L.Hypermedia.Siren.Parse({
+				entities: [
+					{
+						class: [
+							'duration',
+							'due-date'
+						],
+						rel: [
+							'https://api.brightspace.com/rels/date'
+						],
+						properties: {
+							seconds: 6
+						}
+					}
+				]
+			});
+
+			expect(component._sirenClassProperty(entity, 'due-date')).to.equal(6);
+
+		});
+
+		it('Read a completion', () => {
+			var entity =  window.D2L.Hypermedia.Siren.Parse({
+				entities: [
+					{
+						class: [
+							'completion',
+							'due-date'
+						],
+						entities: [
+							{
+								class: [
+									'completion-date',
+									'date'
+								],
+								rel: [
+									'https://api.brightspace.com/rels/date'
+								],
+								properties: {
+									date: '2100-08-01T04:00:00.000Z'
+								}
+							}
+						],
+						rel: [
+							'https://api.brightspace.com/rels/date'
+						]
+					}
+				]
+			});
+
+			expect(component._sirenClassProperty(entity, 'due-date')).to.equal('2100-08-01T04:00:00.000Z');
+
+		});
+
+	});
+
 });
