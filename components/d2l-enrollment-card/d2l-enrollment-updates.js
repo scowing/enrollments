5a24dcab7d666f29acfbacccd689b41d01c65d11
Edward Gao <Edward.Gao@D2L.com>
2019-03-27 13:29:01 -0400

    US104992-componentize enrollment updates

--- components/d2l-enrollment-card/d2l-enrollment-updates.js
+++ components/d2l-enrollment-card/d2l-enrollment-updates.js
@@ -3,93 +3,114 @@
 
 Polymer-based web component for a enrollment updates.
 */
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
-*/
-import '@polymer/polymer/polymer-legacy.js';
-
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
+import 'd2l-polymer-siren-behaviors/store/entity-behavior.js';
 import 'd2l-organizations/components/d2l-organization-updates/d2l-organization-updates.js';
 import 'd2l-card/d2l-card-footer-link.js';
 import 'd2l-tooltip/d2l-tooltip.js';
-import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
-const $_documentContainer = document.createElement('template');
 
-$_documentContainer.innerHTML = `<dom-module id="d2l-enrollment-updates">
-	<template strip-whitespace="">
-		<style>
-			.d2l-enrollment-card-updates-tooltip {
-				@apply --d2l-body-small-text;
-				color: #FFFFFF;
-				white-space:nowrap;
-			}
-			.d2l-enrollment-card-updates-tooltip[disabled] {
-				display: none;
-			}
-			.d2l-enrollment-card-updates-tooltip ul {
-				margin: 0px;
-				padding: 0px;
-				list-style-type: none;
-			}
-			.d2l-enrollment-card-updates-tooltip ul li {
-				margin: 0px;
-				padding: 0px;
-			}
-		</style>
-		<template is="dom-repeat" items="[[_notifications]]">
-			<template is="dom-if" if="[[!item.isDisabled]]">
-				<d2l-card-footer-link id="[[item.key]]" icon="[[item.icon]]" text="[[item.ariaLabel]]" href$="[[item.link]]" secondary-text="[[_toString(item.updateCount)]]">
-				</d2l-card-footer-link>
-				<d2l-tooltip class="d2l-enrollment-card-updates-tooltip" for="[[item.key]]" position="top" disabled$="[[item.isDisabled]]">
-					<ul>
-						<template is="dom-repeat" items="[[item.toolTip]]">
-							<li>[[item]]</li>
-						</template>
-					</ul>
-				</d2l-tooltip>
+/**
+ * @customElement
+ * @polymer
+ */
+class EnrollmentUpdates extends mixinBehaviors([
+	D2L.PolymerBehaviors.Siren.EntityBehavior,
+	D2L.PolymerBehaviors.Organization.Updates.Behavior
+], PolymerElement) {
+	static get template() {
+		return html`
+			<style>
+				.d2l-enrollment-card-updates-tooltip {
+					@apply --d2l-body-small-text;
+					color: #FFFFFF;
+					white-space:nowrap;
+				}
+				.d2l-enrollment-card-updates-tooltip[disabled] {
+					display: none;
+				}
+				.d2l-enrollment-card-updates-tooltip ul {
+					margin: 0px;
+					padding: 0px;
+					list-style-type: none;
+				}
+				.d2l-enrollment-card-updates-tooltip ul li {
+					margin: 0px;
+					padding: 0px;
+				}
+			</style>
+			<template is="dom-repeat" items="[[_notifications]]">
+				<template is="dom-if" if="[[!item.isDisabled]]">
+					<d2l-card-footer-link id="[[item.key]]" icon="[[item.icon]]" text="[[item.ariaLabel]]" href$="[[item.link]]" secondary-text="[[_toString(item.updateCount)]]">
+					</d2l-card-footer-link>
+					<d2l-tooltip class="d2l-enrollment-card-updates-tooltip" for="[[item.key]]" position="top" disabled$="[[item.isDisabled]]">
+						<ul>
+							<template is="dom-repeat" items="[[item.toolTip]]">
+								<li>[[item]]</li>
+							</template>
+						</ul>
+					</d2l-tooltip>
+				</template>
 			</template>
-		</template>
-	</template>
-	
-</dom-module>`;
+		`;
+	}
+	static get properties() {
+		return {
+			showDropboxUnreadFeedback: {
+				type: Boolean,
+				value: false
+			},
+			showUnattemptedQuizzes: {
+				type: Boolean,
+				value: false
+			},
+			showUngradedQuizAttempts: {
+				type: Boolean,
+				value: false
+			},
+			showUnreadDiscussionMessages: {
+				type: Boolean,
+				value: false
+			},
+			showUnreadDropboxSubmissions: {
+				type: Boolean,
+				value: false
+			},
+			_notifications: {
+				type: Array,
+				value: function() { return []; }
+			}
+		};
+	}
 
-document.head.appendChild($_documentContainer.content);
-Polymer({
-	is: 'd2l-enrollment-updates',
+	static get observers() {
+		return [
+			'_loadData(entity)'
+		];
+	}
 
-	properties: {
-		href: String,
-		presentationHref: String,
+	static get is() { return 'd2l-enrollment-updates'; }
 
-		_notifications: {
-			type: Array,
-			value: function() { return []; }
-		}
-	},
+	_loadData(entity) {
+		var presentationAttributes = {
+			'ShowDropboxUnreadFeedback': this.showDropboxUnreadFeedback,
+			'ShowUnattemptedQuizzes': this.showUnattemptedQuizzes,
+			'ShowUngradedQuizAttempts': this.showUngradedQuizAttempts,
+			'ShowUnreadDiscussionMessages': this.showUnreadDiscussionMessages,
+			'ShowUnreadDropboxSubmissions': this.showUnreadDropboxSubmissions,
+		};
 
-	behaviors: [
-		D2L.PolymerBehaviors.Organization.Updates.Behavior
-	],
+		var notification = this._orgUpdates_fetch(this.entity, presentationAttributes);
+		this._notifications = this._orgUpdates_notifications(notification);
+	}
 
-	observers: [
-		'_fetchNotifications(href, presentationHref)'
-	],
-
-	_fetchNotifications: function(notificationsUrl, presentationUrl) {
-		if (!notificationsUrl || !presentationUrl) {
-			return Promise.resolve();
-		}
-		return this._orgUpdates_fetch(notificationsUrl, presentationUrl)
-			.then(function(notification) {
-				this._notifications = this._orgUpdates_notifications(notification);
-			}.bind(this));
-	},
-	_toString: function(a) {
+	_toString(a) {
 		if (typeof a !== String) {
 			return a.toString();
 		}
 
 		return a;
 	}
-});
+}
+
+window.customElements.define(EnrollmentUpdates.is, EnrollmentUpdates);
