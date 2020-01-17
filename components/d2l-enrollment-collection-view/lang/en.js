60d380e4520a71b0dc699ca0c863c357108c23ae
Maya Jones <Maya.Jones@D2L.com>
2020-01-17 16:24:52 -0500

    Language Update

--- components/d2l-enrollment-collection-view/lang/en.js
+++ components/d2l-enrollment-collection-view/lang/en.js
@@ -1,13 +1,13 @@
 /* eslint quotes: 0 */
 
 export default {
-	myLearning: "My Learning", // My learning pages title
-	searchPlaceholder: "Search...", // Placeholder text for the search input to search the list learning items
-	noSearchResults: "There are no courses or learning paths for your search entry.", // The message that is displayed when a search for learning items returns no results
-	noLearning: "You don't have any courses or learning paths to display.", // The message that is displayed when a user has no learning items to show
-	learningPathEnrollmentType: "Learning Path", // The name of the Learning Path org unit type
-	courseEnrollmentType: "Course", //  The name of the Course org unit type
-	loadMore: "Load More", // The button that will load additional learning items,
-	searchCompletedOne: "{count} search results found.", // The announcement when a search returns one page of results
-	searchCompletedPaged: "More than {count} search results found.", // The announcement when a search returns more than one page of results
+	"courseEnrollmentType": "Course", //  The name of the Course org unit type
+	"learningPathEnrollmentType": "Learning Path", // The name of the Learning Path org unit type
+	"loadMore": "Load More", // The button that will load additional learning items,
+	"myLearning": "My Learning", // My learning pages title
+	"noLearning": "You don't have any courses or learning paths to display.", // The message that is displayed when a user has no learning items to show
+	"noSearchResults": "0 search results found for \"{searchTerm}\"", // The message that is displayed when a search for learning items returns no results
+	"searchCompletedOne": "{count} search results found.", // The announcement when a search returns one page of results
+	"searchCompletedPaged": "More than {count} search results found.", // The announcement when a search returns more than one page of results
+	"searchPlaceholder": "Search...", // Placeholder text for the search input to search the list learning items
 };
