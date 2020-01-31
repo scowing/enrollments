0c0d060afa47bb432e8d56df4ffb4c3c83f6222c
Allan Kerr <allan.kerr@D2L.com>
2020-01-31 09:49:00 -0500

    Fix linting failure for fr.js

--- components/d2l-enrollment-collection-view/lang/fr.js
+++ components/d2l-enrollment-collection-view/lang/fr.js
@@ -5,7 +5,7 @@ export default {
 	"learningPathEnrollmentType": "Chemin d’apprentissage", // The name of the Learning Path org unit type
 	"loadMore": "En voir plus", // The button that will load additional learning items,
 	"myLearning": "Mon apprentissage", // My learning pages title
-	"noLearning": "Vous n\'avez aucun cours ou chemin d\'apprentissage à afficher.", // The message that is displayed when a user has no learning items to show
+	"noLearning": "Vous n'avez aucun cours ou chemin d'apprentissage à afficher.", // The message that is displayed when a user has no learning items to show
 	"noSearchResults": "0 résultats de recherche trouvés pour « {searchTerm} »", // The message that is displayed when a search for learning items returns no results
 	"searchCompletedOne": "{count} résultats de recherche trouvés.", // The announcement when a search returns one page of results
 	"searchCompletedPaged": "Plus de {count} résultats de recherche trouvés.", // The announcement when a search returns more than one page of results
