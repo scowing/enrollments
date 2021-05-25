0bb9b5ecdddafc8cac2793dfb31bb3080eaf72d5
JenkinsReader <resupport@desire2learn.com>
2021-05-25 18:10:39 -0400

    Translation Updates

---
+++ components/d2l-enrollment-collection-view/lang/fr-fr.js
@@ -0,0 +1,13 @@
+/* eslint quotes: 0 */
+
+export default {
+	"courseEnrollmentType": "Course", //  The name of the Course org unit type
+	"learningPathEnrollmentType": "Parcours d’apprentissage", // The name of the Learning Path org unit type
+	"loadMore": "Load More", // The button that will load additional learning items,
+	"myLearning": "My Learning", // My learning pages title
+	"noLearning": "Vous n'avez aucun cours ou chemin d'apprentissage à afficher.", // The message that is displayed when a user has no learning items to show
+	"noSearchResults": "Aucun résultat de recherche trouvé pour « {searchTerm} »", // The message that is displayed when a search for learning items returns no results
+	"searchCompletedOne": "{count} search results found.", // The announcement when a search returns one page of results
+	"searchCompletedPaged": "More than {count} search results found.", // The announcement when a search returns more than one page of results
+	"searchPlaceholder": "Rechercher...", // Placeholder text for the search input to search the list learning items
+};
